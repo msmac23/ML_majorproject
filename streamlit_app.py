67e3dd59413d3cfd63e98c6ad786a4621da8fa86
msmac23 <153698329+msmac23@users.noreply.github.com>
2024-08-11 15:15:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title('🎈 Vehicle Prediction ML')
 
-st.info('This is a Machine learning app to predict the prices of used cars on craigslist!')
+st.info('This is a Machine learning app to predict the prices of used cars on craigslist')
 
 st.write('Hello world!')
