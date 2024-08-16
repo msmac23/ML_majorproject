7920eaa2565f0810478c57693bee89f47d134c49
msmac23 <153698329+msmac23@users.noreply.github.com>
2024-08-15 23:51:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,33 +48,12 @@ with st.sidebar:
     'st louis, MO', 'stockton', 'tampa bay area', 'tucson', 'washington, DC'
   )
 
-  years = (
-    1910,	1928,	1929,	1930,	1931,	1935,	1937,	1940,	1947,	1948,	1949,	1952,	1953,	1954,	1955,	1957,	1958, 1959,	
-    1960,	1962,	1963,	1964,	1965,	1966,	1967,	1968,	1969,	1970,	1971,	1972,	1973,	1975,	1976,	1977,	1978,	1979,
-    1980,	1981,	1982,	1983,	1984,	1985,	1986,	1987,	1988,	1989,	1990,	1991,	1992,	1993,	1994,	1995,	1996,	1997,
-    1998,	1999,	2000,	2001,	2002,	2003,	2004,	2005,	2006,	2007,	2008,	2009,	2010,	2011,	2012,	2013,	2014,	2015,
-    2016, 2017,	2018,	2019,	2020,	2021,	2022)
-    
-  # Step 2: Create a slider for selecting a year
-  min_year = min(years)
-  max_year = max(years)
-
-  selected_year = st.slider(
-    'Select a Year',
-    min_value=min_year,
-    max_value=max_year,
-    value=(min(years), max(years)),  # Default range selection
-    step=1
-  )
-
-  # Display the selected year
-  st.write(f'Selected Year: {selected_year}') 
+  #bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
   
-  # bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
-   
 
   # Creating sliders and selectbox for each variable
   region = st.selectbox('Region', regions)
+  year = st.slider('Year of Car', 1910, 2022, 1981) 
   
 
 
