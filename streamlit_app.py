2f92901b7a932b7b5392bdb8a6022945941eda7d
msmac23 <153698329+msmac23@users.noreply.github.com>
2024-08-15 23:41:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,12 +55,26 @@ with st.sidebar:
     1998,	1999,	2000,	2001,	2002,	2003,	2004,	2005,	2006,	2007,	2008,	2009,	2010,	2011,	2012,	2013,	2014,	2015,
     2016, 2017,	2018,	2019,	2020,	2021,	2022)
     
-   # bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+  # Step 2: Create a slider for selecting a year
+  min_year = min(years)
+  max_year = max(years)
+
+  selected_year = st.slider(
+    'Select a Year',
+    min_value=min_year,
+    max_value=max_year,
+    value=(min_year, max_year)  # Default value as a range
+  )
+
+  # Display the selected year
+  st.write(f'Selected Year: {selected_year}') 
+  
+  # bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
    
 
   # Creating sliders and selectbox for each variable
   region = st.selectbox('Region', regions)
-  year = st.slider('Year', years)
+  
 
 
 
