cf15255bc68d9246bc7142d9953af11106d6b1af
msmac23 <153698329+msmac23@users.noreply.github.com>
2024-08-15 22:34:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,13 +20,29 @@ with st.expander('Data'):
   y = vehicles_df.price
   y
 
-# region,price,year,manufacturer,model,condition,cylinders,fuel,odometer,title_status,transmission,type,paint_color
+# Visualization Charts
+
 with st.expander('Data Visualization'):
   st.scatter_chart(data=vehicles_df, x='manufacturer', y='condition', color='fuel') 
   st.scatter_chart(data=vehicles_df, x='condition', y='type', color='transmission')
-  
   st.bar_chart(data=vehicles_df, x='type', y='cylinders', x_label= 'Type', y_label='Cylinders', color='type',
                horizontal=False, stack=None, width=None, height=None, use_container_width=True)
+
+
+# User Input
+with st.sidebar:
+  st.header('Input Features')
+  # region,price,year,manufacturer,model,condition,cylinders,fuel,odometer,title_status,
+  #transmission,type,paint_color,drive_4wd,drive_fwd,drive_rwd,geo_location
+  region = st.selectbox('region'('atlanta',	'austin',	'baltimore',	'boston',	'central NJ',	'charlotte', 'chicago',	'cincinnati',	
+                        'colorado springs',	'columbus',	'dallas / fort worth',	'denver',	'des moines',	'detroit metro',
+                        'ft myers / SW florida',	'grand rapids',	'houston',	'inland empire',	'jacksonville',	'kansas city',
+                        'las vegas',	'long island',	'los angeles',	'milwaukee',	'minneapolis / st paul',	'nashville',
+                        'new york city',	'north jersey',	'oklahoma city',	'orange county',	'orlando',	'others',
+                        'phoenix',	'pittsburgh',	'portland',	'raleigh / durham / CH',	'reno / tahoe',	'rochester',	
+                        'sacramento',	'san diego',	'seattle-tacoma',	'SF bay area',	'south florida',	'south jersey',
+                        'spokane / coeur d'alene',	'st louis, MO',	'stockton',	'tampa bay area',	'tucson',	'washington, DC'))
+
 
 
 
