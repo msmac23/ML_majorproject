feccb34907e8be441a338f93094a63f9df45c023
msmac23 <153698329+msmac23@users.noreply.github.com>
2024-08-15 21:08:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,6 @@ st.info('This is a Machine learning app to predict the prices of used cars on cr
 st.write('Good day Mr. Senior. Please see our attempt at a machine learning algorithm app.')
 
 with st.expander('Data'):
-  st.write(**'Raw Data'**)
+  st.write('**Raw Data**')
   df = pd.read_csv('https://raw.githubusercontent.com/msmac23/ML_majorproject/master/sample_vehicles_data.csv')
   df
