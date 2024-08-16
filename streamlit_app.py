a2f2d680d82f21b8bca49defd1c538fe79d173a6
msmac23 <153698329+msmac23@users.noreply.github.com>
2024-08-15 21:08:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-st.title('🎈 Vehicle Prediction ML')
+st.title('🎈 Vehicle Prediction Algorithm')
 
 st.info('This is a Machine learning app to predict the prices of used cars on craigslist')
 
 st.write('Good day Mr. Senior. Please see our attempt at a machine learning algorithm app.')
 
-df = pd.read_csv('https://raw.githubusercontent.com/msmac23/ML_majorproject/master/sample_vehicles_data.csv')
-df
+with st.expander('Data'):
+  st.write(**'Raw Data'**)
+  df = pd.read_csv('https://raw.githubusercontent.com/msmac23/ML_majorproject/master/sample_vehicles_data.csv')
+  df
